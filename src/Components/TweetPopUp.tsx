7bf7f3dbdf0f18cf6aaf4ea29b26b7f926b27d3c
Jason <121495300+jason21715@users.noreply.github.com>
2023-06-04 06:44:04 -0700

    Add uploading images to tweet, and submitting tweet info to database

--- src/Components/TweetPopUp.tsx
+++ src/Components/TweetPopUp.tsx
@@ -1,9 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { useGlobalContext } from "./AuthContext";
 import "../Styles/TweetPopUp.scss";
 import { RxCross2 } from "react-icons/rx";
 import { FiImage } from "react-icons/fi";
 import { TweetWindowContext } from "./Sidebar";
+import {
+  collection,
+  addDoc,
+  app,
+  getFirestore,
+  ref,
+  getStorage,
+  uploadBytesResumable,
+  getDownloadURL,
+  updateDoc,
+} from "../Firebase.js";
 
 const TweetPopUp = () => {
   // User profile picture
@@ -11,13 +22,101 @@ const TweetPopUp = () => {
   // Textfield with character limit
   // Tweet
   const { tweetWindow, openTweetWindow } = useContext(TweetWindowContext);
+  const [images, setImages] = useState<File[]>([]);
+
   const { user } = useGlobalContext();
+  const imageCount = useRef(0);
+
+  const db = getFirestore(app);
+
+  //Submit Tweet Information to user.
+  const submitTweetFunction = async () => {
+    const userRef = collection(db, "users", `${user?.uid}`, "tweets");
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    const text = document.getElementById("tweetText") as HTMLTextAreaElement;
+    const textValue = text.value;
+
+    const docRef = await addDoc(userRef, {
+      tweetText: { textValue },
+      likes: 0,
+      comments: 0,
+      timestamp: `${hour} : ${minute}, ${month}/${day}/${year}`,
+      images: "",
+    });
+
+    try {
+      const updatedImages = images.map(async (image) => {
+        const filePath = `${user?.uid}/${docRef.id}/${image.name}`;
+        const newImageRef = ref(getStorage(), filePath);
+        const fileSnapshot = await uploadBytesResumable(newImageRef, image);
+
+        const publicImageUrl = await getDownloadURL(newImageRef);
+
+        return {
+          images: publicImageUrl,
+          storageUri: fileSnapshot.metadata.fullPath,
+        };
+      });
+
+      const updatedImagesUrls = await Promise.all(updatedImages);
+
+      await updateDoc(docRef, {
+        images: updatedImagesUrls,
+      });
+    } catch (error: any) {
+      console.error(error.code, error.message);
+    }
+  };
+
+  //Add small images to imgContainer
+  const previewImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+
+    if (
+      target &&
+      target.files &&
+      target.files?.length + imageCount.current > 4
+    ) {
+      alert("You can only upload 4 images");
+      return;
+    }
+
+    if (target && target.files && target.files.length > 0) {
+      const files = Array.from(target.files);
+      const newFilesArray: File[] = [];
+
+      files.forEach((file) => {
+        const reader = new FileReader();
+        imageCount.current = imageCount.current + 1;
+        newFilesArray.push(file);
+
+        reader.onload = function (e: ProgressEvent<FileReader>) {
+          const imageContainer = document.getElementById("tweetImageContainer");
+          if (imageContainer !== null) {
+            const imgElement = document.createElement("img");
+            imgElement.src = e.target?.result as string;
+
+            imageContainer.appendChild(imgElement);
+          }
+        };
+        reader.readAsDataURL(file);
+      });
+
+      setImages(newFilesArray);
+    }
+  };
 
   return (
     <div id="tweetPopUp">
       <div className="tweet-container">
         <RxCross2
-          size={35}
+          size={75}
           className="tweet-container-exit"
           onClick={() => (tweetWindow ? openTweetWindow() : null)}
         />
@@ -29,17 +128,34 @@ const TweetPopUp = () => {
               placeholder="Write your tweet..."
               maxLength={500}
               required
+              id="tweetText"
             ></textarea>
           </form>
         </div>
 
+        <div id="tweetImageContainer"></div>
+
         <div className="tweet-container-bottom">
-          <FiImage
-            size={30}
-            color="#7856ff"
-            className="tweet-container-submit-image"
-          />
-          <button type="submit" className="tweet-container-button">
+          <label htmlFor="tweetFile">
+            <FiImage
+              size={30}
+              color="#7856ff"
+              className="tweet-container-submit-image"
+            />
+          </label>
+          <input
+            name="tweetFile"
+            type="file"
+            id="tweetFile"
+            accept="image/*"
+            onChange={previewImage}
+            multiple
+          ></input>
+          <button
+            onClick={() => submitTweetFunction()}
+            type="submit"
+            className="tweet-container-button"
+          >
             Tweet
           </button>
         </div>
